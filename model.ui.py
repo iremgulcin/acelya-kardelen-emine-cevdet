95fe003fe849a205b9c785a83519345db8c24078
Kardelen Kaya <93514309+KardelenKaya@users.noreply.github.com>
2024-02-16 19:29:11 +0300

    Update model.ui.py

--- model.ui.py
+++ model.ui.py
@@ -2,12 +2,18 @@ import streamlit as st
 import pandas as pd
 import json
 import joblib
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 # json dicti loadla
 with open("line_map.json", "r") as f:
     line_data = json.load(f)
 model = joblib.load("hist_model_tempsiz_tscv.joblib")  # model
 
+veriler_2022 = pd.read_feather("final_set.feather")
+veriler_2022 = veriler_2022.set_index('transition_date') # indexi tarih olarak ayarla
+veriler_2022["month"] = veriler_2022.index.month
+veriler_2022["day"] = veriler_2022.index.day
 
 def create_input_df(selected_date, selected_hour, transfer, line):
     """
@@ -65,7 +71,7 @@ page_icon="U+1F44B"
 selected_date = st.date_input("Tarih seçiniz:")
 time_options = [str(x) + ":00" for x in range(0, 24)]
 selected_hour = st.selectbox('Saat seçiniz:', time_options)
-selected_hour = selected_hour[0]
+selected_hour = int(selected_hour.split(":")[0])
 #selected_hour = st.slider("Saat seçiniz:", 0, 23, 4)  # Use slider for hour selection
 transfer = st.selectbox("Transfer türünü seçiniz:", ["Normal", "Aktarma"])
 line = st.selectbox("Hat seçiniz:", list(line_data.keys()))
@@ -81,9 +87,36 @@ if st.button("Submit"):
         
         if len(prediction.shape) == 1:
             predicted_passengers = abs(prediction[0])  # single value
+        
         else:
             predicted_passengers = abs(prediction)[0]  # first prediction
         st.write("Tahmin edilen yolcu yoğunluğu:", int(predicted_passengers))
+        
+        # Plot number_of_passenger for the selected line on the same day in 2022
+        selected_month = input_df['month'].iloc[0]
+        selected_day = input_df['day_of_month'].iloc[0]
+        line_same_day_data = veriler_2022[(veriler_2022['line'] == line) & (veriler_2022['month'] == selected_month) & (veriler_2022['day'] == selected_day)]
+        
+                # Plot using Plotly Express
+        fig = px.line(line_same_day_data, x='transition_hour', y='number_of_passenger', 
+                    labels={'number_of_passenger': 'Kullanım Sayısı'},
+                    title=f"{line} Hattı İçin {selected_month} / {selected_day} / 2022'deki Kullanım Dağılımı")
+
+        # Add labels and legend
+        fig.update_layout(
+            xaxis_title='Saat',
+            yaxis_title='Kullanım Sayısı',
+            legend_title='Legend',
+            showlegend=True,
+            hovermode = "x"  
+        )
+
+        fig.update_traces(hovertemplate='Saat: %{x} <br> Kullanım Sayısı: %{y} <extra></extra>')
+
+        # Show the plot using Streamlit
+        st.plotly_chart(fig)
+
 
     except Exception as e:
         st.error(f"Error making prediction: {e}")
+
